1566fd15f55ba3408f3894691822853f803ec645
Tauras Slipavicius <tauras.slipavicius@e3p.de>
2021-02-21 19:45:30 +0200

    Removed old code

--- EmployeeAPI/Domain/Employee.cs
+++ EmployeeAPI/Domain/Employee.cs
@@ -11,9 +11,10 @@ namespace EmployeeAPI.Domain
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
         public DateTime EmploymentDate { get; set; }
-        public Employee Boss { get; set; }
         public string Address { get; set; }
         public double Salary { get; set; }
         public string Role { get; set; } // Should be enum
+
+        public Employee Boss { get; set; }
     }
 }
